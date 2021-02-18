3a59fd90fe2d9fbc45a8cd722fcf086eaa4b86a8
贺永胜 <1378431028@qq.com>
2021-02-18 14:34:09 +0800

    first commit

---
+++ src/views/public/login/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <span>登录</span>
+    <router-link to="/register">注册</router-link>
+    <br />
+    <input type="text" placeholder="请输入账号" v-model="formData.username" />
+    <input
+      type="password"
+      placeholder="请输入密码"
+      v-model="formData.password"
+    />
+    <button>登录</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {}
+    }
+  },
+  mounted() {}
+}
+</script>
+
+<style></style>
