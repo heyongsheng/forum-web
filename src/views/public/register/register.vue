6f81e8debe0c128191076d2c39e6f18d27e46a7c
贺永胜 <1378431028@qq.com>
2021-02-18 14:38:31 +0800

    添加简版注册页面

---
+++ src/views/public/register/register.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <router-link to="/login">登录</router-link>
+    <span>注册</span>
+    <br />
+    <input type="text" placeholder="请输入账号" v-model="formData.username" />
+    <input
+      type="password"
+      placeholder="请输入密码"
+      v-model="formData.password"
+    />
+    <button>登录</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {}
+    }
+  },
+  mounted() {}
+}
+</script>
+
+<style></style>
